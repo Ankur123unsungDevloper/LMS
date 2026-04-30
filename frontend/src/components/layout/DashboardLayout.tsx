e7c638c9b347f968530ecc7499d1abe7e4ef7cd9
roshan-jameer <skroshanjameer325@gmail.com>
2026-04-30 22:10:31 +0530

    Dashboard module added

--- frontend/src/components/layout/DashboardLayout.tsx
+++ frontend/src/components/layout/DashboardLayout.tsx
@@ -1,8 +1,14 @@
+import React from 'react';
+import Sidebar from './Sidebar'; // Make sure this path is correct
+import Navbar from './Navbar';   // Make sure this path is correct
+
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="min-h-screen bg-[#070b14] text-white font-sans flex">
       {/* Assuming Sidebar might go here later */}
-      <div className="flex-1 flex flex-col min-w-0">
+      <Sidebar />
+      <div className="flex-1 flex flex-col min-w-0 ml-64">
+        <Navbar />
         <main className="flex-1 px-6 py-12 overflow-y-auto">
           {children}
         </main>
